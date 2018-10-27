c8733fdfc9246cbe9f42839625df6ea1b443f73e
kirankumar Rajendran <kirankumar.meadiga@gmail.com>
2018-10-27 17:30:49 +0530

    search implemented

--- app/src/main/java/wooqer/com/wooqer/MainActivity.java
+++ app/src/main/java/wooqer/com/wooqer/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import java.util.List;
 
@@ -13,23 +14,48 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import wooqer.com.wooqer.model.Movie;
 import wooqer.com.wooqer.model.MoviesResponse;
+import wooqer.com.wooqer.model.MoviesSearchResponse;
 import wooqer.com.wooqer.network.ApiClient;
 import wooqer.com.wooqer.network.ApiInterface;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private final static String API_KEY = "3f869e81c300cd939e9602d34f616ee8";
+    SearchView myFilter_ads;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        myFilter_ads= (SearchView) findViewById(R.id.filter_ads_txt);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        ApiInterface apiService =
+        final ApiInterface apiService =
                 ApiClient.getClient().create(ApiInterface.class);
 
+        getMovieList(recyclerView, apiService);
+        myFilter_ads.setIconifiedByDefault(false);
+        myFilter_ads.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if (s.length() >= 3) {
+                    getMovieList(recyclerView, apiService);
+                }else {
+                    getMovieSearch(recyclerView, apiService,s);
+                }
+                return true;
+            }
+        });
+
+    }
+
+    private void getMovieList(final RecyclerView recyclerView, ApiInterface apiService) {
         Call<MoviesResponse> call = apiService.getMoviesList(API_KEY);
         call.enqueue(new Callback<MoviesResponse>() {
             @Override
@@ -43,7 +69,22 @@ public class MainActivity extends AppCompatActivity {
                 Log.e(TAG, t.toString());
             }
         });
+    }
+    private void getMovieSearch(final RecyclerView recyclerView, ApiInterface apiService,String msearch) {
+        Call<MoviesSearchResponse> call = apiService.getMovieSearch(msearch,API_KEY);
+        call.enqueue(new Callback<MoviesSearchResponse>() {
+            @Override
+            public void onResponse(Call<MoviesSearchResponse> call, Response<MoviesSearchResponse> response) {
+                response.body();
+                List<Movie> movies = response.body().getResults();
+                recyclerView.setAdapter(new MoviesAdapter(movies, getApplicationContext()));
+            }
 
+            @Override
+            public void onFailure(Call<MoviesSearchResponse> call, Throwable t) {
+                Log.e(TAG, t.toString());
+            }
+        });
     }
 
-    }
+}
--- app/src/main/java/wooqer/com/wooqer/network/ApiInterface.java
+++ app/src/main/java/wooqer/com/wooqer/network/ApiInterface.java
@@ -2,12 +2,13 @@ package wooqer.com.wooqer.network;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 import wooqer.com.wooqer.model.MoviesResponse;
-
-
+import wooqer.com.wooqer.model.MoviesSearchResponse;
 public interface ApiInterface {
     @GET("list/509ec17b19c2950a0600050d")
     Call<MoviesResponse> getMoviesList(@Query("api_key") String apiKey);
-
+    @GET("search/movie/")
+    Call<MoviesSearchResponse> getMovieSearch(@Query("query") String apiKey,@Query("api_key") String query);
 }
